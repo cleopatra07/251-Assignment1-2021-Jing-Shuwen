80edbd73b9d0fffc3647ca112eefd59a2bb47d7d
ssszheng <302871958@qq.com>
2021-08-11 15:21:26 +1200

    Formatted SearchTest.java

--- src/test/myTextEditor/SearchTest.java
+++ src/test/myTextEditor/SearchTest.java
@@ -24,42 +24,40 @@ public class SearchTest {
 		keyLong = "123k";
 	}
 
-	
 	@Test
 	public void testHighlightSingleWord() {
 		Search.highlight(text, keySingle);
 		Highlighter hlighter = text.getHighlighter();
 		Highlighter.Highlight[] hlights = hlighter.getHighlights();
-		Assertions.assertEquals(hlights.length,4);
+		Assertions.assertEquals(hlights.length, 4);
 		for (int i = 0; i < hlights.length; i++) {
-			String actual = text.getText().substring(hlights[i].getStartOffset(),hlights[i].getEndOffset());
-			Assertions.assertEquals(keySingle,actual);
-		}	
+			String actual = text.getText().substring(hlights[i].getStartOffset(), hlights[i].getEndOffset());
+			Assertions.assertEquals(keySingle, actual);
+		}
 	}
+
 	@Test
 	public void testHighlightLongWord() {
 		Search.highlight(text, keyLong);
 		Highlighter hlighter = text.getHighlighter();
 		Highlighter.Highlight[] hlights = hlighter.getHighlights();
-		Assertions.assertEquals(hlights.length,1);
+		Assertions.assertEquals(hlights.length, 1);
 		for (int i = 0; i < hlights.length; i++) {
-			String actual = text.getText().substring(hlights[i].getStartOffset(),hlights[i].getEndOffset());
-			Assertions.assertEquals(keyLong,actual);
-		}	
+			String actual = text.getText().substring(hlights[i].getStartOffset(), hlights[i].getEndOffset());
+			Assertions.assertEquals(keyLong, actual);
+		}
 	}
-	
-	
+
 	@AfterEach
 	public void tearDown(TestInfo testInfo) {
-    	System.out.println("Finished..." + testInfo.getDisplayName());
+		System.out.println("Finished..." + testInfo.getDisplayName());
 		System.out.println("Running: tearDown");
-		text =null;
+		text = null;
 		keySingle = null;
 		keyLong = null;
-        assertNull(text);
-        assertNull(keySingle);
-        assertNull(keyLong);
-		}
-	
+		assertNull(text);
+		assertNull(keySingle);
+		assertNull(keyLong);
+	}
 
 }
